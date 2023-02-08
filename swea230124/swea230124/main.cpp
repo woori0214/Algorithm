77b9336ed78a4dd22b530c7e6508dc37ec86599c
woori <woori50300@gmail.com>
2023-02-08 16:12:21 +0900

    solve.swea 23/01/24

---
+++ swea230124/swea230124/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  swea230124
+//
+//  Created by WOORI JEON on 2023/02/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
