186ee5e7674d6ce151454fb6da9fec2f5c0b83fa
woori <woori50300@gmail.com>
2023-04-21 12:30:35 +0900

    solve: baekjoon1991

--- baekjoon1167/main.py
+++ baekjoon1167/main.py
@@ -1,6 +1,33 @@
 N = int(input())
-tree = [[]for i in range(N+1)]
+tree = {}
 
 for i in range(N):
-    x, y, z = input().split()
-    print(x,y,z)
+    root, left, right = map(str, input().split())
+    tree[root] = [left, right]
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
+
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
